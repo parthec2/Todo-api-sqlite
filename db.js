f64f2d05f67003727ffb6876a831c125557d9211
parth shah ec2 <parth.ec2@gmail.com>
2017-06-23 07:57:10 +0530

    Add postgresql for production

--- db.js
+++ db.js
@@ -1,4 +1,20 @@
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL,{
+        'dialect':'postgres'    
+    });
+
+}else{
+     sequelize = new Sequelize(undefined,undefined,undefined,{
+    'dialect':'sqlite',
+    'storage': __dirname + '/data/dev-todo-api.sqlite' 
+    });
+
+}
+
 var sequelize = new Sequelize(undefined,undefined,undefined,{
     'dialect':'sqlite',
     'storage': __dirname + '/data/dev-todo-api.sqlite' 
