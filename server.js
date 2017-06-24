4d55d2605265bf3814a179d58708fe99c95ce86e
parth shah ec2 <parth.ec2@gmail.com>
2017-06-24 17:26:18 +0530

    Add PUT method with sequenlize

--- server.js
+++ server.js
@@ -145,31 +145,70 @@ app.delete('/todos/:id', function(req, res) {
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
+	//var todoId = parseInt(req.params.id, 10);
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+	// var body = _.pick(req.body, 'description', 'completed');
+	// var validAttributes = {};
+
+	// if (!matchedTodo) {
+	// 	return res.status(404).send();
+	// }
+
+	// if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+	// 	validAttributes.completed = body.completed;
+	// } else if (body.hasOwnProperty('completed')) {
+	// 	return res.status(400).send();
+	// }
+
+	// if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+	// 	validAttributes.description = body.description;
+	// } else if (body.hasOwnProperty('description')) {
+	// 	return res.status(400).send();
+	// }
+
+	// _.extend(matchedTodo, validAttributes);
+	// res.json(matchedTodo);
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
-	var body = _.pick(req.body, 'description', 'completed');
+	var body = _.pick(req.body,"description","completed");
 	var validAttributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
-
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+	if (body.hasOwnProperty('completed')) {
 		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+	if (body.hasOwnProperty('description') ) {
 		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
 	}
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	 db.todo.findById(todoId).then(
+		function(todo)
+		{
+			if(todo){
+				 todo.update(validAttributes)
+				 .then(
+					function(todo)
+					{
+						res.send(todo.toJSON());
+					},
+					function(e)
+					{
+						res.status(400).send();
+					}		 
+     			);//end of then
+
+			}else{
+				return res.status(404).send();
+			}
+		},
+		function()
+		{
+			res.status(500).send();
+		}
+	 );
+
+
 });
 
 db.sequelize.sync().then(function() {
